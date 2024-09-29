3e5bea6fac1526f33bb3244f0fe3352fb66bb660
ajiepanca0 <ajiepanca0@gmail.com>
2024-09-29 19:09:20 +0700

    initial commit

---
+++ src/main/java/com/srss_frontend/SrssFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.srss_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrssFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrssFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srss_frontend/SrssFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srss_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrssFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
